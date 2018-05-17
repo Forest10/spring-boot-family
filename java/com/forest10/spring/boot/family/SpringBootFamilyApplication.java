f3f4636e5293f60224aed97d41176f34c20ca02e
Forest10 <1393679194@qq.com>
2018-05-17 11:56:05 +0800

    init spring jpa

---
+++ java/com/forest10/spring/boot/family/SpringBootFamilyApplication.java
@@ -0,0 +1,19 @@
+package com.forest10.spring.boot.family;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootFamilyApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(this.getClass());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFamilyApplication.class, args);
+	}
+}
---
+++ java/com/forest10/spring/boot/family/api/pojo/JsonResult.java
@@ -0,0 +1,51 @@
+package com.forest10.spring.boot.family.api.pojo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * 描述:
+ * JSON返回结果
+ *
+ * @author Forest10
+ * @date 2018/04/01 16:32
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class JsonResult<T> implements Serializable {
+
+	private static final long serialVersionUID = -8963262177114836103L;
+
+	protected T data;
+
+	protected int status;
+
+	protected String msg;
+
+	@JsonCreator
+	protected JsonResult(@JsonProperty("status") int status, @JsonProperty("msg") String msg, @JsonProperty("data") T data) {
+		this.status = status;
+		this.msg = msg;
+		this.data = data;
+	}
+
+
+	public static <T> JsonResult<T> success(String message, T data) {
+		return new JsonResult<>(0, message, data);
+	}
+
+	public static <T> JsonResult<T> error(String message) {
+		return new JsonResult<>(-1, message, null);
+	}
+
+
+}
