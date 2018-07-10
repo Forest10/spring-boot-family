b451622bed600e94ed10c1f658dca33ed84d8ac8
Forest10 <1393679194@qq.com>
2018-07-10 18:43:01 +0800

    加入AdminServer

--- src/main/java/com/forest10/spring/boot/family/SpringBootFamilyApplication.java
+++ src/main/java/com/forest10/spring/boot/family/SpringBootFamilyApplication.java
@@ -1,10 +1,13 @@
 package com.forest10.spring.boot.family;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootFamilyApplication extends SpringBootServletInitializer {
 
