dfe6821e6edbd0a9036eca0897885b2178f5abe1
Forest10 <1393679194@qq.com>
2018-09-04 17:22:56 +0800

    add index

---
+++ src/main/java/com/forest10/spring/boot/family/service/impl/BasicServiceimpl.java
@@ -0,0 +1,12 @@
+package com.forest10.spring.boot.family.service.impl;
+
+import com.forest10.spring.boot.family.service.BasicService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Forest10
+ * @date 2018/9/4 下午5:22
+ */
+@Service
+public class BasicServiceimpl implements BasicService {
+}
