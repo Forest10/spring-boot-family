109ae0ab60cf6d29b6976240df0869265acab289
Forest10 <1393679194@qq.com>
2018-10-16 19:17:49 +0800

    reformat project

--- src/main/java/com/forest10/spring/boot/family/controller/AliPayController.java
+++ src/main/java/com/forest10/spring/boot/family/controller/AliPayController.java
@@ -1,6 +1,5 @@
 package com.forest10.spring.boot.family.controller;
 
-
 import com.alipay.api.AlipayApiException;
 import com.forest10.spring.boot.family.service.AliPayService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,43 +19,39 @@ import java.io.IOException;
 @RequestMapping("/alipay")
 public class AliPayController {
 
+    @Resource
+    private AliPayService aliPayService;
 
-	@Resource
-	private AliPayService aliPayService;
+    @GetMapping("/return_url")
+    public String returnUrl() {
+        return "return_url";
+    }
 
+    @PostMapping(value = "/notify_url")
+    public String notifyUrl() {
+        return "success";
+    }
 
-	@GetMapping("/return_url")
-	public String returnUrl() {
-		return "return_url";
-	}
+    /**
+     * 手机网站跳转支付(直接调取客户端,如无使用账号填写)
+     *
+     * @param res
+     * @throws IOException
+     */
+    @GetMapping("/wap")
+    public void wapPay(HttpServletResponse res) throws IOException, AlipayApiException {
+        aliPayService.wapPay(res);
+    }
 
-	@PostMapping(value = "/notify_url")
-	public String notifyUrl() {
-		return "success";
-	}
-
-	/**
-	 * 手机网站跳转支付(直接调取客户端,如无使用账号填写)
-	 *
-	 * @param res
-	 * @throws IOException
-	 */
-	@GetMapping("/wap")
-	public void wapPay(HttpServletResponse res) throws IOException, AlipayApiException {
-		aliPayService.wapPay(res);
-	}
-
-
-	/**
-	 * 电脑网站跳转支付(直接调取客户端,如无使用账号填写)
-	 *
-	 * @param res
-	 * @throws IOException
-	 */
-	@GetMapping("/pc")
-	public void pcPay(HttpServletResponse res) throws IOException, AlipayApiException {
-		aliPayService.pcPay(res);
-	}
-
+    /**
+     * 电脑网站跳转支付(直接调取客户端,如无使用账号填写)
+     *
+     * @param res
+     * @throws IOException
+     */
+    @GetMapping("/pc")
+    public void pcPay(HttpServletResponse res) throws IOException, AlipayApiException {
+        aliPayService.pcPay(res);
+    }
 
 }
