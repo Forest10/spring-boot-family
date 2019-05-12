b1f0b41bd6c72d08369c91418878703a62e1226a
sen.shi <sen.shi@yaduo.com>
2019-05-12 18:48:58 +0800

    spring boot纯净版启动

--- src/main/java/com/forest10/spring/boot/family/controller/BasicController.java
+++ src/main/java/com/forest10/spring/boot/family/controller/BasicController.java
@@ -1,12 +1,5 @@
 package com.forest10.spring.boot.family.controller;
 
-import com.forest10.spring.boot.family.common.JsonResult;
-import com.forest10.spring.boot.family.service.IZooHandle;
-import com.forest10.spring.boot.family.service.ZooFactory;
-import javax.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,18 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2018/9/4 下午2:28 占位
  */
 @RestController
-@RequestMapping("/zoo")
 public class BasicController {
-
-
-    @Resource
-    private ZooFactory zooFactory;
-
-    @PostMapping("/quark")
-    public JsonResult quark(@RequestBody ZooReq req) {
-        IZooHandle zooAnimal = zooFactory.getInstance(req.getAnnimalTypeEnum());
-        return JsonResult.success(zooAnimal.quark());
-    }
 
 
 }
